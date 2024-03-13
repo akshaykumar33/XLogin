14fe35b3fc3ac73197895f47680c3f787adcbaf5
Akshaykumar <akshaykumarpatil33@gmail.com>
2024-03-13 11:07:42 +0530

    initial commit

---
+++ xlogin/src/App.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+      <div className="card">
+        </div>
+    </>
+  )
+}
+
+export default App
